11074015089d935c99f6f722edb48c013bab0996
ZHENG Zhong <heavyzheng@gmail.com>
2011-06-17 18:25:05 +0200

    Added test case for child_node_list's node adoption.

--- test/test_element_push.cpp
+++ test/test_element_push.cpp
@@ -78,3 +78,38 @@ BOOST_AUTO_TEST_CASE(test_element_push_texts)
     }
 }
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+BOOST_AUTO_TEST_CASE(test_element_push_adopt)
+{
+    XTREE_LOG_TEST_NAME;
+    const char* TEST_XML = "<root/>";
+    const char* TEST_XML_2 = "<item>content</item>";
+    try
+    {
+        std::auto_ptr<xtree::document> doc(xtree::parse_string(TEST_XML));
+        xtree::element_ptr root = doc->root();
+        const unsigned int MAX_SIZE = 4;
+        for (unsigned int i = 0; i < MAX_SIZE; ++i)
+        {
+            std::auto_ptr<xtree::document> doc2(xtree::parse_string(TEST_XML_2));
+            xtree::element_ptr root2 = doc2->root();
+            root->push_back_adopt(*root2);
+            BOOST_CHECK_EQUAL(doc2->empty(), true);
+        }
+        // At this point, all the 4 documents (doc2) should have been destroyed.
+        for (xtree::child_iterator i = root->begin(); i != root->end(); ++i)
+        {
+            BOOST_CHECK_EQUAL(i->type(), xtree::element_node);
+            BOOST_CHECK_EQUAL(i->name(), "item");
+            BOOST_CHECK_EQUAL(i->content(), "content");
+        }
+    }
+    catch (const xtree::dom_error& ex)
+    {
+        BOOST_ERROR(ex.what());
+    }
+}
+
